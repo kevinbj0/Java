bba7a4dff62c6ff2108aa30dec1430b147085f99
ByoungJin Kim <104419151+kevinbj0@users.noreply.github.com>
2023-07-24 17:20:54 +0900

    0724_commit

---
+++ javaPrj/src/javalang/day1/문자열출력.java
@@ -0,0 +1,12 @@
+package javalang.day1;
+
+public class 문자열출력 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "홍길동";
+		char nameAt = name.charAt(0);
+		System.out.println(nameAt);
+	}
+
+}
